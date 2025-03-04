230143a1387a0d69679dcd09389d357bd6352c60
DanielYLim <daniel.yongho.lim@gmail.com>
2025-03-04 10:56:35 -0500

    Lab 1 structure is updated

--- Module 3/L4/Lab_assignment/autograder/Lab_answer_test.r
+++ Module 3/L4/Lab_assignment/autograder/Lab_answer_test.r
@@ -25,25 +25,25 @@ test_that(" question 2.2", {
 
 test_that(" question 3.1", {
   
-  expect_equal( mean_sample, 2.872222, tolerance=1e-3) 
+  expect_equal( mean_sample, 0.0983, tolerance=1e-3) 
   
 })
 
 test_that(" question 3.2", {
   
-  expect_equal( sd_sample, 0.1550281, tolerance=1e-3) 
+  expect_equal( sd_sample, 0.09516389, tolerance=1e-3) 
   
 })
 
 test_that(" question 4.1", {
   
-  expect_equal( mean_sample2, 2.865067, tolerance=1e-3) 
+  expect_equal( mean_sample2, 0.09834, tolerance=1e-3) 
   
 })
 
 test_that(" question 4.2", {
   
-  expect_equal( sd_sample2, 0.07170299, tolerance=1e-3) 
+  expect_equal( sd_sample2, 0.0419357, tolerance=1e-3) 
   
 })
 
