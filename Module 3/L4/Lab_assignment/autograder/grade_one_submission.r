bf819ed0cfda92385468f3ed55833a59b6c7a30a
DanielYLim <daniel.yongho.lim@gmail.com>
2025-04-02 15:01:36 -0400

    Lab-branch prep to rebase to the main branch

--- Module 3/L4/Lab_assignment/autograder/grade_one_submission.r
+++
@@ -1,6 +0,0 @@
-# load in the package
-library(gradeR)
-
-calcGradesForGradescope("Lab4.R",       # each student's submission must be named this!
-                        "Lab_answer_test.r") # the file with all of the testthat tests 
-  
