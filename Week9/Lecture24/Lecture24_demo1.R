a01aadec6b2f417afc3b80ae60939748ce3bff21
Lucia V <luciamvila@gmail.com>
2025-08-25 17:49:42 -0400

    First draft of Lecture 24 R demos

---
+++ Week9/Lecture24/Lecture24_demo1.R
@@ -0,0 +1,48 @@
+# ---- 0. Setup and load libraries, if any ----
+
+library(ggplot2)
+
+
+# ---- 1. Guinness example: testing assumptions & sampling distributions ----
+
+# Reminder: what is our null hypothesis in this example?
+
+# Let's create a function to run experiments under the null hypothesis:
+
+experiment <- function(N, mu_0, mu, sigma) {
+  # Simulate generating samples under the null hypothesis:
+  samples <- rnorm(N, mean=mu, sd=sigma) 
+  
+  Xbar <- mean(samples)          # take the sample mean
+  Z <- (Xbar - mu_0) / sigma**2  # compute the Z score
+  return(Z)
+}
+
+# 2. ---- Run the experiment 1000 times for N=100, mu_0=mu ----
+
+data1 = as.data.frame(replicate(n=1000, experiment(100, 4.5, 4.5, 0.17)))
+colnames(data1) <- c("Z")
+
+# Does this histogram look familiar?
+ggplot(data1, aes(x=Z)) +
+  geom_histogram(aes(y=..density..), alpha=0.5) +
+  stat_function(fun=dnorm, args=list(mean=0.0, sd=1.0), col="blue") +
+  xlim(-3,3)
+
+# FIXME: normalize the y-axis
+
+
+# 3. ---- Now run the experiment 1000 times for N=5, mu_0=mu ----
+
+data2 = as.data.frame(replicate(n=1000, experiment(5, 4.5, 4.5, 0.17)))
+colnames(data2) <- c("Z")
+
+# Now what's wrong with this histogram?
+ggplot(data2, aes(x=Z)) +
+  geom_histogram(aes(y=..density..), alpha=0.5) +
+  stat_function(fun=dnorm, args=list(mean=0.0, sd=1.0), col="blue") +
+  xlim(-3,3)
+
+# FIXME: normalize the y-axis
+
+# Looks like we'll need a new distribution!
