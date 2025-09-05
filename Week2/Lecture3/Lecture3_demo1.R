e443b829a31481af6368ee30c8c32b142b0984a7
Emily Stephen <emilyps14@gmail.com>
2025-09-05 17:12:06 -0400

    First pass revisions for all lectures

--- Week2/Lecture3/Lecture3_demo1.R
+++ Week2/Lecture3/Lecture3_demo1.R
@@ -9,11 +9,11 @@ setwd(dirname(getSourceEditorContext()$path))  # set working directory
 source("Lecture3_demoFunctions.R")
 
 # ---- 1. Generate data ----
-
+# The "dataset" column indicates which dataset each observation corresponds to
 data1 <- generate_data1()
 head(data1)
 
-# Count the number of rows for each dataset
+# Count the number of observations for each dataset
 table(data1$dataset)
 
 
@@ -50,47 +50,4 @@ ggplot(data=data1, aes(x=x, fill=as.factor(dataset))) +
     labels = c(mean = "Mean", median = "Median")
   ) +
   guides(fill = "none")
-
-# ---- 2. Comparing shapes of distributions and their outliers ----
-
-# Let's try looking at distributions with different variances:
-data2 <- generate_data2()
-head(data2)
-
-table(data2$dataset)
-
-# Look at the distributions
-ggplot(data=data2, aes(x=x, fill=as.factor(dataset))) +
-  geom_histogram() +
-  facet_wrap(~dataset, ncol=1) +
-  guides(fill = "none")
-
-# Compute summary statistics for central tendency and variability
-data2_summary <- data2 %>%
-  group_by(dataset) %>%
-  summarise(
-    mean = round(mean(x),3),
-    variance = var(x),
-    std_dev = sd(x),
-    Q1 = quantile(x, 0.25),
-    median = median(x),
-    Q3 = quantile(x, 0.75),
-    IQR = IQR(x)
-  )
-print(data2_summary)
-
-
-# Plot the distributions again, with lines to show mean+/-2SD
-plot <- histograms_plusminus2sd(data2)
-print(plot)
-
-# We can also look at the IQRs:
-plot2 <- histograms_plusIQR(data2)
-print(plot2)
-
-# What if the distributions are skewed?
-data3 <- generate_data3()
-
-print(histograms_plusminus2sd(data3))
-print(histograms_plusIQR(data3))
 
