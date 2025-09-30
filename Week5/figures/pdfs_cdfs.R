4e50d9f091fcece85021d2262b256cfacdbd30b5
Emily Stephen <emilyps14@gmail.com>
2025-09-30 11:56:52 -0400

    Moving figures to a week-level directory and editing the Geometric CDF to start at 0

--- Week5/figures/pdfs_cdfs.R
+++ Week5/figures/pdfs_cdfs.R
@@ -45,11 +45,14 @@ ggsave("Geometric_PMF.png", geom_pmf_plot, width = 4, height = 2.5, units = "in"
 
 # Geometric CDF
 cdf <- pgeom(outcomes - 1, prob = p)
-theoretical_cdf <- data.frame(outcomes, cumulative = cdf)
+theoretical_cdf <- data.frame(
+  outcomes = c(0, outcomes),
+  cumulative = c(0, cdf)
+)
 
 geom_cdf_plot <- ggplot(data = theoretical_cdf, aes(x = outcomes, y = cumulative)) +
   geom_step(direction = "hv", size = 1.2, color = "darkorange") +
-  scale_x_continuous(breaks = 1:n_max) +
+  scale_x_continuous(breaks = c(0, 1:n_max)) +
   labs(title = "Geometric Distribution CDF (p = 0.35)",
        x = "Trial of First Success",
        y = "Cumulative Probability")
