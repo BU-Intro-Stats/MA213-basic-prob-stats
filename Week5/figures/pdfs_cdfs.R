fa4a632ccb7de772b6b6cc364e15200c60734f26
Emily Stephen <emilyps14@gmail.com>
2025-10-03 10:44:14 -0400

    Changes to Lecture 14 before class

--- Week5/figures/pdfs_cdfs.R
+++ Week5/figures/pdfs_cdfs.R
@@ -88,3 +88,64 @@ binom_cdf_plot <- ggplot(binom_cdf_df, aes(x = successes, y = cumulative)) +
        x = "Number of Successes",
        y = "Cumulative Probability")
 ggsave("Binomial_CDF.png", binom_cdf_plot, width = 4, height = 2.5, units = "in") 
+
+# Binomial PMF with Normal on top
+n_binom2 <- 30
+binom_outcomes2 <- 0:n_binom2
+binom_pmf2 <- dbinom(binom_outcomes2, size = n_binom2, prob = p)
+binom_pmf_df2 <- data.frame(
+  successes = binom_outcomes2,
+  probability = binom_pmf2
+)
+
+mu <- n_binom2 * p
+sigma <- sqrt(n_binom2 * p * (1 - p))
+x_vals <- seq(-0.5, n_binom2 + 0.5, length.out = 1000)
+normal_curve <- dnorm(x_vals, mean = mu, sd = sigma)
+
+binom_pmf_normal_plot <- ggplot(binom_pmf_df2, aes(x = successes, y = probability)) +
+  geom_bar(stat = "identity", fill = "skyblue") +
+  stat_function(fun = function(x) dnorm(x, mean = mu, sd = sigma),
+                color = "black", size = 1.2, n = 1000) +
+  scale_x_continuous(
+    breaks = binom_outcomes2[binom_outcomes2 %% 2 == 0]
+  ) +
+  labs(title = paste0("Binomial PMF with Normal Approximation\n(n = ", n_binom2, ", p = ", p, ")"),
+       x = "Number of Successes",
+       y = "Probability / Density")
+
+ggsave("Binomial_PMF_Normal.png", binom_pmf_normal_plot, width = 4, height = 2.5, units = "in")
+
+# Poisson PMF
+lambda <- 3.5
+n_pois <- 12
+pois_outcomes <- 0:n_pois
+
+pois_pmf <- dpois(pois_outcomes, lambda = lambda)
+pois_pmf_df <- data.frame(
+  outcomes = pois_outcomes,
+  probability = pois_pmf
+)
+
+pois_pmf_plot <- ggplot(pois_pmf_df, aes(x = outcomes, y = probability)) +
+  geom_bar(stat = "identity", fill = "skyblue") +
+  scale_x_continuous(breaks = pois_outcomes) +
+  labs(title = paste0("Poisson PMF (λ = ", lambda, ")"),
+       x = "Number of Events",
+       y = "Probability")
+ggsave("Poisson_PMF.png", pois_pmf_plot, width = 4, height = 2.5, units = "in")
+
+# Poisson CDF
+pois_cdf <- ppois(pois_outcomes, lambda = lambda)
+pois_cdf_df <- data.frame(
+  outcomes = c(-0.5, pois_outcomes, n_pois + 0.5),
+  cumulative = c(0, pois_cdf, 1)
+)
+
+pois_cdf_plot <- ggplot(pois_cdf_df, aes(x = outcomes, y = cumulative)) +
+  geom_step(direction = "hv", size = 1.2, color = "darkorange") +
+  scale_x_continuous(breaks = pois_outcomes) +
+  labs(title = paste0("Poisson CDF (λ = ", lambda, ")"),
+       x = "Number of Events",
+       y = "Cumulative Probability")
+ggsave("Poisson_CDF.png", pois_cdf_plot, width = 4, height = 2.5, units = "in")
